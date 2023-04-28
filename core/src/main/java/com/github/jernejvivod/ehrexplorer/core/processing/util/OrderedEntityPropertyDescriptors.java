be7e18eaf52d20f10dc51fc98d3c5b1e78767fa3
Jernej Vivod <vivod.jernej@gmail.com>
2023-04-28 11:35:46 +0200

    [feat][#153] com.github.jernejvivod.ehrexplorer.core.processing.util.OrderedEntityPropertyDescriptors should support properties without order annotations

--- core/src/main/java/com/github/jernejvivod/ehrexplorer/core/processing/util/OrderedEntityPropertyDescriptors.java
+++ core/src/main/java/com/github/jernejvivod/ehrexplorer/core/processing/util/OrderedEntityPropertyDescriptors.java
@@ -5,6 +5,7 @@ import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -19,7 +20,6 @@ import javax.ws.rs.InternalServerErrorException;
 import com.github.jernejvivod.ehrexplorer.annotation.PropertyOrder;
 import com.github.jernejvivod.ehrexplorer.core.util.EntityUtils;
 
-// TODO refactor, add support for unannotated properties
 @Singleton
 public class OrderedEntityPropertyDescriptors
 {
@@ -34,61 +34,96 @@ public class OrderedEntityPropertyDescriptors
         for (EntityType<?> entityType : em.getMetamodel().getEntities())
         {
             // initialize mapping of property names to associated property position
-            Map<String, Integer> propertyNameToPosition = new HashMap<>();
+            Map<String, Integer> propertyNameToPosition = getPropertyNameToPosition(entityType);
 
-            Set<Field> fields = EntityUtils.getFieldsUpToObject(entityType.getJavaType());
-
-            for (Field declaredField : fields)
-            {
-                if (!declaredField.isAnnotationPresent(PropertyOrder.class))
-                {
-                    throw new InternalServerErrorException("Annotation %s not specified for property '%s' of entity '%s'".formatted(
-                            PropertyOrder.class.getName(),
-                            declaredField.getName(),
-                            entityType.getJavaType().getSimpleName()
-                    ));
-                }
-
-                // set position of property based on annotation
-                int propertyPosition = declaredField.getAnnotation(PropertyOrder.class).value();
-
-                if (propertyPosition <= 0 || propertyPosition > fields.size())
-                {
-                    throw new InternalServerErrorException("Wrong position specified for property '%s' of entity '%s'. Specified position must be between 1 and %s.".formatted(
-                            declaredField.getName(),
-                            entityType.getJavaType().getSimpleName(),
-                            fields.size()
-                    ));
-                }
-
-                if (propertyNameToPosition.containsValue(propertyPosition))
-                {
-                    throw new InternalServerErrorException("Position %d of property '%s' of entity '%s' has already been specified.".formatted(
-                            propertyPosition,
-                            declaredField.getName(),
-                            entityType.getJavaType().getSimpleName()
-                    ));
-                }
-
-                propertyNameToPosition.put(declaredField.getName(), propertyPosition);
-            }
-
-            // put property descriptors in order
-            PropertyDescriptor[] propertyDescriptorsOrdered = new PropertyDescriptor[fields.size()];
-            try
-            {
-                for (PropertyDescriptor propertyDescriptor : Introspector.getBeanInfo(entityType.getJavaType(), Object.class).getPropertyDescriptors())
-                {
-                    propertyDescriptorsOrdered[propertyNameToPosition.get(propertyDescriptor.getName()) - 1] = propertyDescriptor;
-                }
-            }
-            catch (IntrospectionException ex)
-            {
-                throw new InternalServerErrorException(ex);
-            }
+            // get ordered property descriptors
+            PropertyDescriptor[] propertyDescriptorsOrdered = getOrderedPropertyDescriptors(propertyNameToPosition, entityType);
 
             this.classToOrderedPropertyDescriptors.put(entityType.getJavaType(), propertyDescriptorsOrdered);
         }
+    }
+
+    /**
+     * Get mapping from property names to their order for entity.
+     *
+     * @param entityType entity's type
+     * @return mapping for property names to their ordering
+     */
+    private static Map<String, Integer> getPropertyNameToPosition(EntityType<?> entityType)
+    {
+        Map<String, Integer> propertyNameToPosition = new HashMap<>();
+
+        // names of properties without PropertyOrder annotations
+        Set<String> withoutOrderAnnotation = new HashSet<>();
+
+        Set<Field> fields = EntityUtils.getFieldsUpToObject(entityType.getJavaType());
+
+        // map property names to their positions
+        for (Field declaredField : fields)
+        {
+            if (!declaredField.isAnnotationPresent(PropertyOrder.class))
+            {
+                withoutOrderAnnotation.add(declaredField.getName());
+                continue;
+            }
+
+            // set position of property based on annotation
+            int propertyPosition = declaredField.getAnnotation(PropertyOrder.class).value();
+
+            if (propertyPosition <= 0 || propertyPosition > fields.size())
+            {
+                throw new InternalServerErrorException("Wrong position specified for property '%s' of entity '%s'. Specified position must be between 1 and %s.".formatted(
+                        declaredField.getName(),
+                        entityType.getJavaType().getSimpleName(),
+                        fields.size()
+                ));
+            }
+
+            if (propertyNameToPosition.containsValue(propertyPosition))
+            {
+                throw new InternalServerErrorException("Position %d of property '%s' of entity '%s' has already been specified.".formatted(
+                        propertyPosition,
+                        declaredField.getName(),
+                        entityType.getJavaType().getSimpleName()
+                ));
+            }
+
+            propertyNameToPosition.put(declaredField.getName(), propertyPosition);
+        }
+
+        // assign top positions to properties without order annotations
+        int maxAssigned = propertyNameToPosition.values().stream().max(Integer::compareTo).orElse(0);
+        for (String p : withoutOrderAnnotation)
+        {
+            propertyNameToPosition.put(p, maxAssigned++);
+        }
+
+        return propertyNameToPosition;
+    }
+
+    /**
+     * Get {@link PropertyDescriptor} instances for entity's properties based on property ordering annotation values.
+     *
+     * @param propertyNameToPosition mapping of property names to their positions (extracted from {@link PropertyOrder} annotations)
+     * @param entityType type of entity for which to get the ordered property descriptors
+     * @return array of ordered {@link PropertyDescriptor} instances for entity
+     */
+    private static PropertyDescriptor[] getOrderedPropertyDescriptors(Map<String, Integer> propertyNameToPosition, EntityType<?> entityType)
+    {
+        // put property descriptors in order
+        PropertyDescriptor[] propertyDescriptorsOrdered = new PropertyDescriptor[propertyNameToPosition.size()];
+        try
+        {
+            for (PropertyDescriptor propertyDescriptor : Introspector.getBeanInfo(entityType.getJavaType(), Object.class).getPropertyDescriptors())
+            {
+                propertyDescriptorsOrdered[propertyNameToPosition.get(propertyDescriptor.getName()) - 1] = propertyDescriptor;
+            }
+        }
+        catch (IntrospectionException ex)
+        {
+            throw new InternalServerErrorException(ex);
+        }
+        return propertyDescriptorsOrdered;
     }
 
     public PropertyDescriptor[] getForEntity(Class<?> entityClass)
