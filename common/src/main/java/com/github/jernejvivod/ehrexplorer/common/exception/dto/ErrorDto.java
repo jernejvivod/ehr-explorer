c2811585994e4814b4c21ed3f8611b44001b1cb9
Jernej Vivod <vivod.jernej@gmail.com>
2023-04-28 10:19:52 +0200

    [feat][#153] Implement exception mappers

---
+++ common/src/main/java/com/github/jernejvivod/ehrexplorer/common/exception/dto/ErrorDto.java
@@ -0,0 +1,16 @@
+package com.github.jernejvivod.ehrexplorer.common.exception.dto;
+
+public class ErrorDto
+{
+    private String message;
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+}
